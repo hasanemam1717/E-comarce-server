c89c7e78929892464da00a28164e4524ab982bff
hasanemam1717 <hasanemam1717@gmail.com>
2024-11-21 01:12:21 +0600

    setUp for work

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000 
@@ -15,6 +16,31 @@ app.use(express.json())
 
 
 // mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9fglmuq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  const dbConnect =async()=>{
+    try{
+        client.connect();
+        console.log("Successfully connected database");
+
+    }
+    catch(err){
+        console.log("Error name",err.name,err.massage);
+
+    }
+  }
+
+  dbConnect()
+
+
 
 
 
